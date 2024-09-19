957515c9b859ad3a1fea7765aaf2620015f18b43
mari-guazz <guazzinha@gmail.com>
2024-09-19 17:31:11 -0300

    first commit

---
+++ example/data.js
@@ -0,0 +1,29 @@
+let dataPN = [
+    {
+        id: 1,
+        title: "",
+        description: "",
+        data: "",
+        thumb: "",
+    },
+
+    {
+        id: 2,
+        title: "",
+        description: "",
+        data: "",
+        thumb: "",
+    }
+]
+
+let dataUN = [
+    {
+        id: 1,
+        thumb: ""
+    },
+
+    {
+        id: 2,
+        thumb: ""
+    }
+]
